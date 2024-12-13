156baf8ead00e9af76dfd01b3437ebafba222bb3
miraceti <elecam@gmail.com>
2024-12-14 00:10:12 +0100

    est

---
+++ ctk_001.py
@@ -0,0 +1,15 @@
+import customtkinter
+
+#theme
+customtkinter.set_appearance_mode("dark")  # Modes: "System" (standard), "Dark", "Light"
+customtkinter.set_default_color_theme("blue")  # Themes: "blue" (standard), "green", "dark-blue"
+
+root = customtkinter.CTk()
+
+root.title('CustomTK')
+root.geometry('600x350')
+
+button_1 = customtkinter.CTkButton(master=root, text="Hello world!")
+button_1.pack(pady=50, padx=50)
+
+root.mainloop()
